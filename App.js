fbfd8724b1695c3e3f362fe3ab614816d1786cf8
o7lipy00 <o7lipy00@students.oamk.fi>
2024-08-30 17:20:37 +0300

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+const [age, setAge] = useState('')
+const [lowerlimit, setLowerLimit] = useState(0)
+const [upperlimit, setUpperLimit] = useState(0)
+
+
+
+const calculatebothlimits =() =>{
+  const resultlower = (220 - age) * 0.65
+
+setLowerLimit (resultlower)
+const resultupper = (220 - age) * 0.85
+setUpperLimit (resultupper)
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+      
+      <TextInput style={styles.field} 
+      placeholder ='set your age here'
+      value ={age}
+      onChangeText={text => setAge(text)}
+      keyboardType='decimal-pad'
+      />
+
+
+      <Text style= {styles.field}>Limits</Text>
+      <Text style= {styles.field}>{lowerlimit.toFixed(0) }-{upperlimit.toFixed(0) } </Text>
+     <Button title = 'Calculate' onPress={calculatebothlimits}></Button>
+    
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +43,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems : 'flex-start',
+   paddingTop: 60,
+   margin: 20,
   },
+  field:{
+  marginTop: 8,
+  marginBottom: 8,
+}
 });
